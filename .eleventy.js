167f1c584166b446e1c56ca6c62fb8e8959cc76e
unknown <borybuth@gmail.com>
2024-02-13 22:53:46 -0500

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,30 @@
+const Card = require('./src/_includes/components/Card');
+
+module.exports = function(eleventyConfig){
+    eleventyConfig.addPassthroughCopy("src/assets/");
+    eleventyConfig.addPassthroughCopy("src/css/");
+    eleventyConfig.addPassthroughCopy("src/js/");
+    eleventyConfig.addWatchTarget("src/css/");
+    eleventyConfig.addWatchTarget("src/js/");
+
+    eleventyConfig.addShortcode("Card", Card);
+
+    return {
+        templateFormats: [
+            "md",
+            "njk",
+            "html",
+        ],
+
+        //pathPrefix: "/",
+        markdownTemplateEngine: "njk",
+        htmlTemplateEngine: "njk",
+        //dataTemplateEngine: false,
+
+        dir: {
+            input: "src",
+            includes: "_includes",
+            output: "_site",
+        }
+    };
+}
